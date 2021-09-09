2a99f97faadc99d68a01965e107dd0a8250fa651
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-09-09 11:58:34 +0200

    Preparation TP3 :construction:

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,7 @@
 <?php
 
-require 'users.php';
+require 'bdd.php';
+$db = new PDO("mysql:host=localhost;dbname=" . DB_NAME, DB_USER, DB_PASS);
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST')
     header('Location: signin.php');
@@ -9,16 +10,15 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
     $login = htmlentities($_POST['login']);
     $password = htmlentities($_POST['password']);
     session_start();
+    $stmt = $db->prepare('select * from users where login = ? AND password = ?');
+    $user = $stmt->execute([$login, $password]);
+    // Problème à corriger : renvoie true à chaque fois
+    print_r($user);
 
-    $found = false;
-    foreach ($users as $log => $pass) {
-        if ($login === $log && $password === $pass) {
-            $found = true;
-            $_SESSION['username'] = $login;
-            header('Location: welcome.php');
-        }
-    }
-    if (!$found) {
+    if ($user) {
+        $_SESSION['username'] = $login;
+        //header('Location: welcome.php');
+    } else {
         $_SESSION['message'] = 'Nom d\'utilisateur ou mot de passe incorrect(s)';
         header('Location: signin.php');
     }
---
+++ bdd.php
@@ -0,0 +1,4 @@
+<?php
+const DB_NAME = "PWeb";
+const DB_USER = "root";
+const DB_PASS = "";
