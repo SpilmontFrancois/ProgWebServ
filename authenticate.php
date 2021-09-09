d581c0277f42af4a027717f667068270131de88e
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-09-09 14:52:02 +0200

    Bug fix :bug:

--- authenticate.php
+++ authenticate.php
@@ -1,23 +1,26 @@
 <?php
 
 require 'bdd.php';
-$db = new PDO("mysql:host=localhost;dbname=" . DB_NAME, DB_USER, DB_PASS);
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST')
     header('Location: signin.php');
+
+$db = new PDO("mysql:host=localhost;dbname=" . DB_NAME, DB_USER, DB_PASS);
 
 if (isset($_POST['login']) && isset($_POST['password'])) {
     $login = htmlentities($_POST['login']);
     $password = htmlentities($_POST['password']);
     session_start();
-    $stmt = $db->prepare('select * from users where login = ? AND password = ?');
-    $user = $stmt->execute([$login, $password]);
-    // Problème à corriger : renvoie true à chaque fois
-    print_r($user);
+
+    $stmt = $db->prepare('select login from users where login = ? AND password = ?');
+    $stmt->execute([$login, $password]);
+    foreach ($stmt as $row) {
+        $user = $row;
+    }
 
     if ($user) {
         $_SESSION['username'] = $login;
-        //header('Location: welcome.php');
+        header('Location: welcome.php');
     } else {
         $_SESSION['message'] = 'Nom d\'utilisateur ou mot de passe incorrect(s)';
         header('Location: signin.php');
