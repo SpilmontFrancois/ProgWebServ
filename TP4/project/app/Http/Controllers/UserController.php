ea2cb3614eff97c1b0ec73f1611c04a98be845dd
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-09-21 09:48:18 +0200

    ✨ Add main eloquent structure

--- TP4/project/app/Http/Controllers/UserController.php
+++ TP4/project/app/Http/Controllers/UserController.php
@@ -7,16 +7,20 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(Request $request)
     {
-        return $this->collectionPaginate(UserResource::collection(
-            User::simplePaginate($request->input('paginate') ?? 15)
-        ), 'Users loaded');
+        return UserResource::collection(User::simplePaginate($request->input('paginate') ?? 15));
+    }
+
+    public function show(int $id) {
+        try {
+            $user = User::findOrFail($id);
+            return json_encode(new UserResource($user));
+        } catch (Exception $e) {
+            return json_encode('User does not exist.');
+        }
     }
 }
