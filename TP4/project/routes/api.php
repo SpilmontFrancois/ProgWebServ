ac5f71aba9983e065db69fc28fbadc963d15b5b4
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-09-28 11:02:19 +0200

    ✨ Add new routes

--- TP4/project/routes/api.php
+++ TP4/project/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnnonceController;
+use App\Http\Controllers\FriendController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +31,6 @@ Route::get('/', function () {
 
 Route::apiResource('users', UserController::class);
 Route::apiResource('messages', MessageController::class);
+Route::apiResource('annonces', AnnonceController::class);
+Route::apiResource('friends', FriendController::class);
+Route::apiResource('groups', GroupController::class);
