7da5fa609be2ccc449713f51c8f9a2f0308c5cf2
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2021-11-01 21:17:42 +0100

    modif

--- TP4/project/api/routes/api.php
+++ TP4/project/api/routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept, charset, boundary, Content-Length');
 header('Access-Control-Allow-Origin: *');
 
